1bb4fc7e1dfe99bc0ac0ed9c19203f19ec9581be
王一凡0000 <2893084319@qq.com>
2020-12-15 18:48:35 +0800

    提交user-service.xml

---
+++ kmall-user-service/src/main/java/com/kgc/kmall/user/service/MemberService.java
@@ -0,0 +1,13 @@
+package com.kgc.kmall.user.service;
+
+import com.kgc.kmall.bean.Member;
+
+import java.util.List;
+
+/**
+ * @author wangyifan
+ * @create 2020-12-15 18:45
+ */
+public interface MemberService {
+ List<Member> getMemberlist();
+}
