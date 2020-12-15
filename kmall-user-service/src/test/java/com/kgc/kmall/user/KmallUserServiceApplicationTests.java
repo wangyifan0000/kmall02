a44b8880b08bbfce705fe741542c5474aea63bc3
王一凡0000 <2893084319@qq.com>
2020-12-15 19:07:38 +0800

    提交user-web.xml

--- kmall-user-service/src/test/java/com/kgc/kmall/user/KmallUserServiceApplicationTests.java
+++ kmall-user-service/src/test/java/com/kgc/kmall/user/KmallUserServiceApplicationTests.java
@@ -1,13 +1,21 @@
 package com.kgc.kmall.user;
 
+import com.kgc.kmall.bean.Member;
+import com.kgc.kmall.user.mapper.MemberMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 @SpringBootTest
 class KmallUserServiceApplicationTests {
-
+@Resource
+	MemberMapper memberMapper;
 	@Test
 	void contextLoads() {
+		List<Member> members = memberMapper.selectByExample(null);
+		System.out.println(members.size());
 	}
 
 }
